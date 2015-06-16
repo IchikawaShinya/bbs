b1275d1864bec77fcb751b1dbec8a679b801410c
ichikawa <ichikawa@AirHiroki14.local>
2015-06-16 18:59:58 +0900

    バリデーションコメント修正

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,5 @@
 class Response < ActiveRecord::Base
-  validates :comment, presence: true
+  validates :comment, presence: { message: "コメントが入力されていません。" }
   
   default_scope -> { order("response_num ASC") }
 end
