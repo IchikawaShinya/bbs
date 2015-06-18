34eab79f34b80d6fdc06529fbe9892d3f49c33e6
ichikawa <ichikawa@AirHiroki14.local>
2015-06-18 17:52:07 +0900

    バリデータ設定

--- app/models/thread_board.rb
+++ app/models/thread_board.rb
@@ -1,2 +1,5 @@
 class ThreadBoard < ActiveRecord::Base
+  validates :category_id, presence: { message: "カテゴリが選択されていません。" }
+  validates :thread_name, presence: { message: "スレッド名が入力されていません。" }
+  validates :delete_pass, presence: { message: "削除用パスワードが入力されていません。" }
 end
