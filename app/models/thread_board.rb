062e586bcf881cc47a74d4ed14b640f8137b539f
ichikawa <ichikawa@AirHiroki14.local>
2015-06-19 18:54:22 +0900

    バリデータメッセージ修正

--- app/models/thread_board.rb
+++ app/models/thread_board.rb
@@ -1,5 +1,9 @@
 class ThreadBoard < ActiveRecord::Base
-  validates :category_id, presence: { message: "カテゴリが選択されていません。" }
-  validates :thread_name, presence: { message: "スレッド名が入力されていません。" }
-  validates :delete_pass, presence: { message: "削除用パスワードが入力されていません。" }
+  has_many :responses, dependent: :destroy
+  
+  validates :category_id, presence: { message: "が選択されていません。" }
+  validates :thread_name, presence: { message: "が入力されていません。" }
+  validates :delete_pass, presence: { message: "が入力されていません。" }
+  
+  default_scope -> { order("id ASC") }
 end
