b9411922b69ac50b71c1576bbfa1f1c08a3b8ef6
ichikawa <ichikawa@AirHiroki14.local>
2015-06-24 11:19:17 +0900

    assetsファイル設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  
+  config.assets.precompile += ['*.js']
+  config.assets.precompile += ['*.scss']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
