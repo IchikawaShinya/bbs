6b269e70b303c8bf7397bbe73fb3293e97b173fa
ichikawa <ichikawa@AirHiroki14.local>
2015-07-03 11:22:58 +0900

    rspec修正

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,20 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Response, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   
-      thread_board_id = 1
-      limit_count = 3
-  
-  describe 'response' do
-    it 'getResponses' do
-      @responses = Response.getResponses(thread_board_id)
-      expect(@responses.length).to > 0
+  describe 'response' do 
+    #  スレッドID
+    thread_board_id = 1
+    #  投稿件数
+    response_count = 5
+    #  表示件数
+    limit_count = 3
+    
+    before do
+      response_count.times do |i|
+        response = Response.new(
+        "response_num"=> i,
+        "thread_board_id"=> "1",
+        "user_name"=> "user1",
+        "user_email"=> "test@test.com",
+        "user_ipaddress"=> "192.168.0.1",
+        "comment"=> "投稿テスト"+i.to_s)
+        response.save
+      end
     end
     
+    # 全件取得
+    it 'getResponses' do
+      @responses = Response.getResponses(thread_board_id)
+      expect(@responses.length).to eq response_count
+    end
+    
+    # 指定数取得
     it 'getLimitResponses' do
       @responses = Response.getLimitResponses(thread_board_id,limit_count)
-      expect(@responses.length).to eq 3
+      expect(@responses.length).to eq limit_count
     end
   end
 end
