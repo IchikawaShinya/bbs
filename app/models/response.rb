49e6e8f762fea5f823ea05aeac8d79205a271fe2
ichikawa <ichikawa@AirHiroki14.local>
2015-07-16 19:05:28 +0900

    データ取得修正

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,23 +2,27 @@ class Response < ActiveRecord::Base
   soft_deletable dependent_associations: [:thread_boards]
   soft_deletable :column => :deleted_at
   
-  belongs_to :thread_boards
+  belongs_to :thread_board
   validates :comment, presence: { message: "が入力されていません。" }
   
   default_scope -> { order("responses.response_num ASC") }
   
   def self.getResponses (thread_board_id)
-    @responses = Response.where("thread_board_id = ?", thread_board_id)
+    @responses = Response.joins(:thread_board).select('"responses".*, "thread_boards"."category_id"').where("thread_board_id = ?", thread_board_id)
     
     return @responses
   end
   
   def self.getLimitResponses (thread_board_id, limit_count)
       # 件数取得
-      @responses = Response.where("thread_board_id = ?", thread_board_id).reorder("responses.response_num DESC").limit(limit_count)
+      @responses = Response.joins(:thread_board).select('"responses".*, "thread_boards"."category_id"').where("thread_board_id = ?", thread_board_id).reorder("responses.response_num DESC").limit(limit_count)
       
-      # 投稿順に並び替え
-      @responses = @responses.sort
+      binding.pry
+      
+      unless @responses.blank?
+        # 投稿順に並び替え
+        @responses = @responses.sort
+      end
       
       return @responses
   end
