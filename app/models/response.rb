1d2479269a2442563e564fc552ea46248a552410
ichikawa <ichikawa@AirHiroki14.local>
2015-07-17 10:17:08 +0900

    デバッグコード削除修正

--- app/models/response.rb
+++ app/models/response.rb
@@ -17,8 +17,6 @@ class Response < ActiveRecord::Base
       # 件数取得
       @responses = Response.joins(:thread_board).select('"responses".*, "thread_boards"."category_id"').where("thread_board_id = ?", thread_board_id).reorder("responses.response_num DESC").limit(limit_count)
       
-      binding.pry
-      
       unless @responses.blank?
         # 投稿順に並び替え
         @responses = @responses.sort
